bf555435e10d9cf555bee7df2d5f79156336f5e4
Mahdjour Oussama <76874664+mahdjourOussama@users.noreply.github.com>
2025-01-28 15:11:43 +0100

    update database service

--- services/db/init.sql
+++ services/db/init.sql
@@ -1,1 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS vector;
+-- Optional: create a database table using the vector type
+CREATE TABLE IF NOT EXISTS example_table (
+    id SERIAL PRIMARY KEY,
+    embedding VECTOR(3)
+);
